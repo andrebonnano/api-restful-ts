8199f36fe2c397fc7717ecf262765d522855b0fe
Andre Bonnano <andrebonnano@hotmail.com>
2022-08-01 19:06:22 -0300

    commit inicial

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import config from "config"
+import router from "./router"
+
+const app = express()
+
+//midleware
+app.use(express.json)
+
+//rotas
+app.use("/api/", router)
+
+//definição da porta
+const port = config.get<number>("port")
+app.listen(port, async () =>{
+    console.log(`Aplicação rodando na porta: ${port}`)
+})
